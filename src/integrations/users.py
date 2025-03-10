19f36619e4ec32d455d8e8e085ed3cb63d72be40
Михаил <mxoilslukin@gmail.com>
2025-03-11 00:23:18 +0500

    CARD-49: added cache

--- src/integrations/users.py
+++ src/integrations/users.py
@@ -3,6 +3,9 @@ from uuid import UUID
 
 import httpx
 from helpers.clients.http_client import BaseApiClient
+from helpers.redis_client.client import RedisClient
+
+from src.settings import get_settings
 
 
 class UsersClient(BaseApiClient):
@@ -10,16 +13,25 @@ class UsersClient(BaseApiClient):
 
     async def get_users(self, ids: list[UUID]) -> dict[UUID, dict[str, None]] | list[dict[Any, dict[str, Any]]]:
         url = f'{self._base_url}/api/users/'
-        for user_id in ids:
-            url += f'&user__id={user_id}'
+        user_data = {}
+        with RedisClient(address=get_settings().redis.url, db=get_settings().redis.users_db) as rc:
+            for user_id in ids:
+                data = await rc.get(user_id)
+                if data is not None:
+                    user_data[user_id] = data
+                    continue
+                url += f'&user__id={user_id}'
         response = await self.get(url)
-        result = {}
         try:
             response.raise_for_status()
         except httpx.HTTPError:
             for user_id in ids:
-                result[user_id] = {'first_name': None, 'last_name': None}
-            return result
-        for item in response.json()['data']:
-            result[item['id']] = {'first_name': item['first_name'], 'last_name': item['last_name']}
-        return result
+                if user_id not in user_data:
+                    user_data[user_id] = {'first_name': None, 'last_name': None}
+            return user_data
+        with RedisClient(address=get_settings().redis.url, db=get_settings().redis.users_db) as rc:
+            for item in response.json()['data']:
+                data = {'first_name': item['first_name'], 'last_name': item['last_name']}
+                rc.set(item['id'], data)
+                user_data[item['id']] = data
+        return user_data
